68124a1ea78ba45bb63ab73d248dc06ebc9fd9b3
hasnat_kallol <hasnatkallol1971@gmail.com>
2025-02-28 15:08:54 +0600

    Ok

--- js/cashout.js
+++ js/cashout.js
@@ -1,46 +1,54 @@
 document.getElementById('cash-out').addEventListener('click',function(e){
-    e.preventDefault();
+  e.preventDefault();
 
-    let mainBalance = convertInnerText('main-balance')   
-    let convertCashAmount = convertValue('cash-out-input');
-    let convertPin = convertValue('pinInput');
-    let convertAcountNumber = document.getElementById('acount-number').value;
-    let currentTime = new Date().toLocaleTimeString();
-    let transactionId = randaomTransactionId();
+  let mainBalance = convertInnerText('main-balance')   
+  let convertCashAmount = convertValue('cash-out-input');
+  let convertPin = convertValue('pinInput');
+  let convertAcountNumber = document.getElementById('acount-number').value;
+  let currentTime = new Date().toLocaleTimeString();
+  let transactionId = randaomTransactionId();
 
-    if( mainBalance < convertCashAmount){
-        alert("Invalid Amount")
-        return
-    }
-    if(convertAcountNumber.length === 11){
-         if(convertPin === 1234){
-            sum = mainBalance - convertCashAmount ;
-            setInnerTextValue('main-balance',sum)
+  if( mainBalance < convertCashAmount){
+      alert("Invalid Amount")
+      return
+  }
+  if(convertAcountNumber.length === 11){
+       if(convertPin === 1234){
+          sum = mainBalance - convertCashAmount ;
+          setInnerTextValue('main-balance',sum)
 
-            let container = document.getElementById('container');
-            let div = document.createElement('div');
-            div.innerHTML = 
-            `
-            <div class="bg-off-white border border-gray-300 p-4 rounded-lg shadow-sm m-4">
-              <div class="flex gap-6  items-center">
-                <img src="./assets/image/financial 1.png" class="w-16" alt="">
-                 <div>
-                    <div>
-                       <h1>Cash Out ${convertCashAmount} Acount Number : ${convertAcountNumber}</h1>
-                         <p class = "text-red-300">Transaction Id: ${transactionId}</p>
-                         <p>Time : ${currentTime}</p>
-                    </div>
+          document.getElementById('cart-modal2').classList.remove('hidden')
+
+          let container = document.getElementById('container');
+          let div = document.createElement('div');
+          div.innerHTML = 
+          `
+          <div class="bg-off-white border border-gray-300 p-4 rounded-lg shadow-sm m-4">
+            <div class="flex gap-6  items-center">
+              <img src="./assets/image/financial 1.png" class="w-16" alt="">
+               <div>
+                  <div>
+                     <h1>Cash Out ${convertCashAmount} Acount Number : ${convertAcountNumber}</h1>
+                       <p class = "text-red-300">Transaction Id: ${transactionId}</p>
+                       <p>Time : ${currentTime}</p>
                   </div>
-               </div>
+                </div>
              </div>
-            `
-            container.appendChild(div)
-         }
-         else{
-            alert('Wrong PAssword')
-         }
+           </div>
+          `
+          container.appendChild(div)
+
+        
        }
-     else{
-       alert('Invalid Acount Number')
+       else{
+          alert('Wrong PAssword')
+       }
      }
+   else{
+     alert('Invalid Acount Number')
+   }
 })
+
+document.getElementById('close2').addEventListener('click',function(){
+  document.getElementById('cart-modal2').classList.add('hidden')
+  })
--- js/main.js
+++ js/main.js
@@ -1,85 +1,86 @@
+
 document.getElementById('add-money').addEventListener('click',function(e){
-    e.preventDefault();
+  e.preventDefault();
 
-    let mainBalance = convertInnerText('main-balance')   
-    let convertAmount = convertValue('amount-add');
-    let convertPin = convertValue('pinInput');
-    let convertAcountNumber = document.getElementById('acount-number').value;
-    let allBank = document.getElementById('bank').value;
-    let currentTime = new Date().toLocaleTimeString();
-    let transactionId = randaomTransactionId();
+  let mainBalance = convertInnerText('main-balance')   
+  let convertAmount = convertValue('amount-add');
+  let convertPin = convertValue('pinInput');
+  let convertAcountNumber = document.getElementById('acount-number').value;
+  let allBank = document.getElementById('bank').value;
+  let currentTime = new Date().toLocaleTimeString();
+  let transactionId = randaomTransactionId();
+
+
   
+  if(convertAmount <= 0){
+      alert("Invalid Amount")
+      return
+  }
 
-    
-    if(convertAmount <= 0){
-        alert("Invalid Amount")
-        return
-    }
+  if(convertAcountNumber.length === 11){
+       if(convertPin === 1234){
 
-    if(convertAcountNumber.length === 11){
-         if(convertPin === 1234){
+          let sum = mainBalance + convertAmount ;
+          setInnerTextValue('main-balance',sum)
 
-            let sum = mainBalance + convertAmount ;
-            setInnerTextValue('main-balance',sum)
-
-            document.getElementById('cart-modal').classList.remove('hidden')
-            
-            let container = document.getElementById('container');
-            let div = document.createElement('div');
-            div.innerHTML = 
-            `
-            <div class="bg-off-white border border-gray-300 p-4 rounded-lg shadow-sm m-4">
-              <div class="flex gap-6  items-center">
-                <img src="./assets/image/financial 1.png" class="w-16" alt="">
-                 <div>
-                    <div>
-                       <h1 class=''>Added Money from ${allBank} </h1>
-                       <p>Acount Number : ${convertAcountNumber}</p>
-                       <p class = "text-red-300">Transaction Id: ${transactionId}</p>
-                       <p>Time : ${currentTime}</p>
-                    </div>
+          document.getElementById('cart-modal').classList.remove('hidden')
+          
+          let container = document.getElementById('container');
+          let div = document.createElement('div');
+          div.innerHTML = 
+          `
+          <div class="bg-off-white border border-gray-300 p-4 rounded-lg shadow-sm m-4">
+            <div class="flex gap-6  items-center">
+              <img src="./assets/image/financial 1.png" class="w-16" alt="">
+               <div>
+                  <div>
+                     <h1 class=''>Added Money from ${allBank} </h1>
+                     <p>Acount Number : ${convertAcountNumber}</p>
+                     <p class = "text-red-300">Transaction Id: ${transactionId}</p>
+                     <p>Time : ${currentTime}</p>
                   </div>
-               </div>
+                </div>
              </div>
-            `
-            container.appendChild(div);
+           </div>
+          `
+          container.appendChild(div);
 
 
 
-            let popap = document.getElementById('cart-popap');
-            let divElement = document.createElement('div');
-            divElement.innerHTML = 
-            `
-            <div class="bg-off-white border border-gray-300 p-4 rounded-lg shadow-sm m-4">
-              <div class="flex gap-6  items-center">
-                
-                 <div>
-                    <div>
-                       <h1 class=''>Added Money from ${allBank} </h1>
-                       <p>Acount Number : ${convertAcountNumber}</p>
-                       <p class = "text-red-300">Transaction Id: ${transactionId}</p>
-                       <p>Time : ${currentTime}</p>
-                    </div>
+          let popap = document.getElementById('cart-popap');
+          let divElement = document.createElement('div');
+          divElement.innerHTML = 
+          `
+          <div class="bg-off-white border border-gray-300 p-4 rounded-lg shadow-sm m-4">
+            <div class="flex gap-6  items-center">
+              
+               <div>
+                  <div>
+                     <h1 class=''>Added Money from ${allBank} </h1>
+                     <p>Acount Number : ${convertAcountNumber}</p>
+                     <p class = "text-red-300">Transaction Id: ${transactionId}</p>
+                     <p>Time : ${currentTime}</p>
                   </div>
-               </div>
+                </div>
              </div>
-            `
-            popap.appendChild(divElement);
+           </div>
+          `
+          popap.appendChild(divElement);
 
-         }
-         
-
-         else{
-            alert('Wrong PAssword')
-         }
        }
-     else{
-       alert('Invalid Acount Number')
+       
+
+       else{
+          alert('Wrong PAssword')
+       }
      }
+   else{
+     alert('Invalid Acount Number')
+   }
 })
 
 
 
-        document.getElementById('close').addEventListener('click',function(){
-          document.getElementById('cart-modal').classList.add('hidden')
-    })
+      document.getElementById('close').addEventListener('click',function(){
+        document.getElementById('cart-modal').classList.add('hidden')
+        })
